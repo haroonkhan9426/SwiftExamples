563383607d77f18baf5bf7057dbf1be089fac323
Haroon khan <haroonkhan9426@gmail.com>
2019-04-15 19:18:51 +0500

    Initial Commit

---
+++ testProject/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  testProject
+//
+//  Created by EAPPLE on 15/04/2019.
+//  Copyright © 2019 Code Valley. All rights reserved.
+//
+
+import Foundation
+
+var testString:String = "this is string";
+var testInt:Int = 25;
+var testFloat:Float = 25.9;
+var tesstDouble:Double = 23.444403;
+let testConstant = "Constant";
+print("Hello, World!");
+print("String: \(testString)");
+print("Int: \(testInt)");
+print("Float: \(testFloat)");
+print("Double: \(tesstDouble)");
+print("Constant: \(testConstant)");
+
